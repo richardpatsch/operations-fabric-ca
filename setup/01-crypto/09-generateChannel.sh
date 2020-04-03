747cb3bf599693007e659e52b0b38517ba4db02a
Richard <rpa@richards-mbp.home>
2020-04-04 00:49:19 +0200

    Add admincerts to org2 adminMSP

--- setup/01-crypto/09-generateChannel.sh
+++ setup/01-crypto/09-generateChannel.sh
@@ -7,6 +7,13 @@ mv crypto/org1/peer1/tls-msp/keystore/* crypto/org1/peer1/tls-msp/keystore/key.p
 mv crypto/org1/peer2/tls-msp/keystore/* crypto/org1/peer2/tls-msp/keystore/key.pem
 mv crypto/org2/peer1/tls-msp/keystore/* crypto/org2/peer1/tls-msp/keystore/key.pem
 mv crypto/org2/peer2/tls-msp/keystore/* crypto/org2/peer2/tls-msp/keystore/key.pem
+
 mkdir -p crypto/org1/admin/msp/admincerts
 cp crypto/org1/admin/msp/signcerts/cert.pem crypto/org1/admin/msp/admincerts
 cp crypto/org0/orderer/tls-msp/tlscacerts/tls-0-0-0-0-8052.pem crypto/org1/peer1/msp
+cp crypto/org0/orderer/tls-msp/tlscacerts/tls-0-0-0-0-8052.pem crypto/org1/peer2/msp
+
+mkdir -p crypto/org2/admin/msp/admincerts
+cp crypto/org2/admin/msp/signcerts/cert.pem crypto/org2/admin/msp/admincerts
+cp crypto/org0/orderer/tls-msp/tlscacerts/tls-0-0-0-0-8052.pem crypto/org2/peer1/msp
+cp crypto/org0/orderer/tls-msp/tlscacerts/tls-0-0-0-0-8052.pem crypto/org2/peer2/msp
