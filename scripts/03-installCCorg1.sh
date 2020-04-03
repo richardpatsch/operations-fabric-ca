1e4eb763fa61ffe22049cb74d85f02fc4a9f8d26
Richard <rpa@richards-mbp.home>
2020-04-03 20:19:55 +0200

    Remove empty lines

--- scripts/03-installCCorg1.sh
+++ scripts/03-installCCorg1.sh
@@ -1,3 +1,1 @@
-
-peer chaincode install -n mycontract -v 1.0 -p github.com/hyperledger/fabric-samples/chaincode/chaincode_example02 
-
+peer chaincode install -n mycontract -v 1.0 -p github.com/hyperledger/fabric-samples/chaincode/chaincode_example02
--- scripts/04-instanciateCCorg1.sh
+++ scripts/04-instanciateCCorg1.sh
@@ -1,3 +1,1 @@
-
 peer chaincode instantiate -o orderer-org0:7050 -C channel1 -n mycontract -v 1.0 -c '{"Args":["init","a","100","b","200"]}' --tls --cafile /tmp/hyperledger/org1/peer1/msp/tls-0-0-0-0-8052.pem
-
