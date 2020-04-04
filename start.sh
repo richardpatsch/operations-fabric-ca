a8e29a7ab878a167b25eb20546efb1e33943b872
Richard <rpa@richards-mbp.home>
2020-04-04 11:09:00 +0200

    Add chaincode setup-scripts to start start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 scriptPath=$PWD/setup/01-crypto
 
+#setup MSPs/artifacts<
 sh $scriptPath/01-enrollCAadmin.sh
 sleep 5
 sh $scriptPath/02-orgOrd.sh
@@ -22,3 +23,22 @@ sh $scriptPath/09-generateChannel.sh
 docker-compose -f docker-compose-peer.yaml up -d
 docker-compose -f docker-compose-peer-ord.yaml up -d
 docker-compose -f docker-compose-cli.yaml up -d
+
+#tactical sleep
+temp_cnt=20
+while [[ ${temp_cnt} -gt 0 ]]; do
+    printf "\rPlease wait %2d second(s) to make sure that all containers are running properly; Hit Ctrl+C to cancel that operation!" ${temp_cnt}
+    sleep 1
+    ((temp_cnt--))
+done
+
+#update scriptPath
+scriptPath=$PWD/setup/02-chaincode
+
+#install chaincode
+sh $scriptPath/01-createchannel.sh
+sh $scriptPath/02-joinchannel.sh
+sh $scriptPath/03-installcc.sh
+sh $scriptPath/04-instantiate.sh
+sh $scriptPath/05-invoke.sh
+sh $scriptPath/06-querycc.sh
