c4d48a8246a575457face7f3785fd159b7bc96b5
Richard <rpa@richards-mbp.home>
2020-04-04 12:55:24 +0200

    Replace sleeps with feedbackSleep function

--- start.sh
+++ start.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
+
+#function for waiting loop with feedback in console
+#arg1: time in seconds
+#arg2: text to display while waiting
+sleepf() {
+    timeinseconds=$1
+    displaytext=$2
+
+    while [[ ${timeinseconds} -gt 0 ]]; do
+        printf "\rPlease wait %2d second(s) %s; Hit Ctrl+C to cancel that operation!" ${timeinseconds} "${displaytext}"
+        sleep 1
+        ((timeinseconds--))
+    done
+}
+
 scriptPath=$PWD/setup/01-crypto
 
-#setup MSPs/artifacts<
+#setup MSPs/artifacts
 sh $scriptPath/01-enrollCAadmin.sh
-sleep 5
+sleepf 3 "cause reasons"
 sh $scriptPath/02-orgOrd.sh
-sleep 5
+sleepf 3 "cause reasons"
 sh $scriptPath/03-org1.sh
-sleep 5
+sleepf 3 "cause reasons"
 sh $scriptPath/04-org2.sh
-sleep 5
+sleepf 3 "cause reasons"
 sh $scriptPath/05-generateCerts-org1.sh
-sleep 5
+sleepf 3 "cause reasons"
 sh $scriptPath/06-generateCerts-org2.sh
-sleep 5
+sleepf 3 "cause reasons"
 sh $scriptPath/07-generateCerts-ord.sh
-sleep 5
+sleepf 3 "cause reasons"
 sh $scriptPath/08-generateMSP.sh
-sleep 5
+sleepf 3 "cause reasons"
 sh $scriptPath/09-generateChannel.sh
 
-docker-compose -f docker-compose-peer.yaml up -d
 docker-compose -f docker-compose-peer-ord.yaml up -d
+docker-compose -f docker-compose-peer.yaml up -d
 docker-compose -f docker-compose-cli.yaml up -d
 
 #tactical sleep
-temp_cnt=20
-while [[ ${temp_cnt} -gt 0 ]]; do
-    printf "\rPlease wait %2d second(s) to make sure that all containers are running properly; Hit Ctrl+C to cancel that operation!" ${temp_cnt}
-    sleep 1
-    ((temp_cnt--))
-done
+sleepf 20 "to make sure all containers are running"
 
 #update scriptPath
 scriptPath=$PWD/setup/02-chaincode
@@ -40,5 +50,11 @@ sh $scriptPath/01-createchannel.sh
 sh $scriptPath/02-joinchannel.sh
 sh $scriptPath/03-installcc.sh
 sh $scriptPath/04-instantiate.sh
+
+sleepf 10 "to wait for proper instantiation"
+
 sh $scriptPath/05-invoke.sh
+
+sleepf 3 "wait for state sync"
+
 sh $scriptPath/06-querycc.sh
